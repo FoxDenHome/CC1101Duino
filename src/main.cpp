e0cfb0c0b36b2fcee0443f206a7af6fe197a74ab
Doridian <git@doridian.net>
2022-01-23 09:12:44 -0800

    Enable watchdog

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 
+#include <avr/wdt.h>
+
 #include "cc1101.h"
 #include "serial_host.h"
 
 void setup() {
+  const uint8_t mcusr_mirror = MCUSR;
+  MCUSR = 0;
+  wdt_disable();
+
   hostSerial.init();
   cc1101.setup();
-  hostSerial.echo(F("CC1101-duino ready"));
+
+  hostSerial.echoFirst(F("CC1101-duino ready "));
+  hostSerial.sendEnd(String(mcusr_mirror));
+  wdt_enable(WDTO_2S);
 }
 
 void loop() {
+  wdt_reset();
+
   hostSerial.loop();
 }
